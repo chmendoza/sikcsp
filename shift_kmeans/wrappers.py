4bf2d2e5cc096a8216ac0287558c2275db4d9873
Carlos H. Mendoza-Cardenas <cmendoza@udel.edu>
2020-01-07 14:32:52 -0500

    Add shift_invariant_pairwise_distances_argmin_min()
    Define shift_invariant_pairwise_distances_argmin_min().

--- shift_kmeans/wrappers.py
+++ shift_kmeans/wrappers.py
@@ -4,6 +4,7 @@ Numpy ans scikit-learn wrappers
 import numpy as np
 
 from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.metrics.pairwise import pairwise_distances_argmin_min
 
 from shift_kmeans.datasets import utils
 
@@ -20,7 +21,7 @@ def shift_invariant_euclidean_distances(kernels, X, X_norm_squared, squared=Fals
     kernels (numpy.ndarray):
         kernels[i] is a kernel with length `kernel_length`.
     X (numpy.ndarray):
-        X[i] is a sample with length `n_features`. 
+        X[i] is a sample with length `n_features`.
         `kernel_length` < `n_features`.
     X_norm_squared (numpy.ndarray):
         Precomputed squared euclidean norm of rows of `X`.
@@ -31,7 +32,7 @@ def shift_invariant_euclidean_distances(kernels, X, X_norm_squared, squared=Fals
     -------
     distances (numpy.ndarray):
         distances[i][j][k] is the euclidean distance from kernels[i], shifted
-        `j` positions, to sample X[k]. 
+        `j` positions, to sample X[k].
     """
 
     n_kernels, kernel_length = kernels.shape
@@ -52,3 +53,40 @@ def shift_invariant_euclidean_distances(kernels, X, X_norm_squared, squared=Fals
                                                 X_norm_squared, squared=True)
 
     return distances
+
+def shift_invariant_pairwise_distances_argmin_min(X, kernels, x_squared_norms):
+    """
+    Shift-invariant wrapper of http://bit.ly/argmin_min_sklearn
+    """
+
+   # euclidean_distances() requires 2D
+    if x_squared_norms.ndim == 1:
+        x_squared_norms = x_squared_norms.reshape(1,-1)
+
+    n_samples, sample_length = X.shape
+    n_kernels, kernel_length = kernels.shape
+    n_shifts = sample_length - kernel_length + 1
+
+    metric_kwargs = {'squared': True, 'X_norm_squared': x_squared_norms}
+
+    # Assume n_shifts <= 2**16 = 65536
+    best_shifts = np.empty((n_kernels, n_samples), dtype=np.uint16)
+    best_distances = np.empty((n_kernels, n_samples))
+
+    # Pad and roll the kernels
+    padded_kernels = np.pad(kernels, [(0, 0), (0, n_shifts-1)],
+                            mode='constant')
+    shifts = np.tile(np.arange(n_shifts, dtype=np.int), (n_kernels, 1))
+    rolled_kernels = utils.roll_rows(padded_kernels, shifts)
+
+    for kernel_id, rolled_kernel in enumerate(rolled_kernels):
+        best_shifts[kernel_id], best_distances[kernel_id] = \
+            pairwise_distances_argmin_min(
+                X=X, Y=rolled_kernel, metric_kwargs=metric_kwargs)
+
+    # For each sample, find closest shifted kernel
+    labels = np.argmin(best_distances, axis=0)
+    best_shifts = best_shifts[labels, np.arange(n_samples)]
+    best_distances = np.min(best_distances, axis=0)
+
+    return labels, best_shifts, best_distances
