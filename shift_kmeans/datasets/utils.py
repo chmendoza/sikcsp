0d0e34bc6a47ec84ae9fe9818737449b17c1bc24
Carlos H. Mendoza-Cardenas <cmendoza@udel.edu>
2020-01-07 14:30:04 -0500

    Improve docstrings of roll_rows
    Improve docstrings of roll_rows. Also, add code to check input shapes.

--- shift_kmeans/datasets/utils.py
+++ shift_kmeans/datasets/utils.py
@@ -4,32 +4,57 @@ Extensions to numpy functions
 
 import numpy as np
 
-def roll_rows(a, shift):
+def roll_rows(array, shift):
     """
-    Roll each row of `a` independently
+    Roll each row of `array` independently
 
-    shift[i] has the offsets to be applied independently to a[i]. `shift` must
-    have the same number of rows than `a`.
+    Parameters
+    ----------
+    array (numpy.ndarray):
+        2D matrix whose rows are to be rolled
+    shift (numpy.ndarray):
+        shift[i] has the offset(s) to be applied independently to a[i].
+
+    Returns
+    -------
+    rolled_array (numpy.ndarray):
+        Array with rolled rows according to shift
+
+    Shapes
+    ------
+    array:          (n_rows, n_cols)
+    shift:          (n_rows, n_shifts)
+    rolled_array:   (n_rows, n_shifts, n_cols)
 
     See https://stackoverflow.com/a/20361561/4292705
     """
 
-    row_indices, column_indices = np.ogrid[:a.shape[0], :a.shape[1]]
+    n_rows, n_cols = array.shape
+
+    row_indices, column_indices = np.ogrid[:array.shape[0], :array.shape[1]]
 
     # Use always a negative shift, so that column_indices are valid.
     # (could also use module operation)
-    shift[shift < 0] += a.shape[1]
+    shift[shift < 0] += n_cols
 
-    if shift.shape[0] == 1:
-        shift = shift.T
+    # Check array shape
+    assert array.ndim == 2, 'The array to be rolled must have two dimensions'
 
+    # Checl shift shape
+    assert shift.ndim == 2, 'The shift array must have two dimensions'
+    assert shift.shape[0] == n_rows,\
+        '`array` and `shifts` must have the same number of rows'
+
+    # Add third dimension to broadcast properly the indexes.
     shift = np.expand_dims(shift, -1)
+
+    # Compute column indexes and broadcast
     column_indices = column_indices - shift
 
-    if column_indices.ndim == 3:
-        row_indices = np.expand_dims(row_indices, -1)
+    # Add third dimension to row indexes for proper broadcasting
+    row_indices = np.expand_dims(row_indices, -1)
 
-    return a[row_indices, column_indices]
+    return array[row_indices, column_indices]
 
 def roll_rows_of_3D_matrix(a, shift):
     """
