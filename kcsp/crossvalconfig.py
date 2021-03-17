b418c6cb3cd166646c63f5766ca6a210e74204e5
Carlos H. Mendoza-Cardenas <cmendoza@udel.edu>
2021-03-17 18:53:29 -0400

    Refactor to get patient and spectral band labels from cli
    Also, compute number of training windows directly from the variables
    saved by splitdata().

--- kcsp/crossvalconfig.py
+++ kcsp/crossvalconfig.py
@@ -1,8 +1,8 @@
 import os, sys
 import yaml
-import itertools
 import numpy as np
 from argparse import ArgumentParser
+
 
 # Add path to package directory to access main module using absolute import
 sys.path.insert(0,
@@ -12,6 +12,8 @@ sys.path.insert(0,
 
 from kcsp import utils
 
+def minusone(x): return x - 1  # Matlab index starts at 1, Python at 0
+
 # Parse command-line arguments
 parser = ArgumentParser()
 parser.add_argument("-k", "--nclusters", dest="n_clusters",
@@ -19,6 +21,11 @@ parser.add_argument("-k", "--nclusters", dest="n_clusters",
 parser.add_argument("-P", "--centroid-lengths", type=int,
                     dest="centroid_lengths", nargs='+', 
                     help="Length of cluster centroids")
+parser.add_argument("--patient", dest="patients",
+                    action='append', nargs='+', help="Patient labels")
+parser.add_argument("--bands", dest="bands", type=int, action="append",
+                    nargs='+', help="Spectral band ids")
+
 args = parser.parse_args()
 
 params = dict.fromkeys(
@@ -32,26 +39,31 @@ params['Algorithm'] = dict.fromkeys(['metric', 'init', 'n_runs', 'n_clusters', '
 n_folds = 10
 params['Crossvalidation']['n_folds'] = n_folds
 
-patients = ['HUP070', 'HUP078']
-data_dir = '/lustre/scratch/cmendoza/sikmeans/LKini2019'
-confdir = '/home/1420/sw/shift_kmeans/kcsp/config'
+patients = args.patients
+# data_dir = '/lustre/scratch/cmendoza/sikmeans/LKini2019'
+# confdir = '/home/1420/sw/shift_kmeans/kcsp/config'
 # patients = ['Study012']
-# data_dir = '/home/cmendoza/Research/sikmeans/LKini2019/data/toy'
-# confdir = '/home/cmendoza/Research/sikmeans/LKini2019/config'
+data_dir = '/home/cmendoza/Research/sikmeans/LKini2019/data/toy'
+confdir = '/home/cmendoza/Research/sikmeans/LKini2019/config'
 methods = ['regular', 'betadiv', 'max-sliced-Bures']
 method = methods[0]
 
 srate = 512 # Hz
 params['Data']['Segment length'] = srate * 60 # 1 minute
 params['Data']['Window length'] = srate * 1  # 1 second
-bands = [[3, 6], [2, 7]]  # best bands with highest AUC (NER'21)
+
+conditions = ['preictal', 'interictal']
+bands = args.bands
+if not isinstance(bands, list):
+    bands = [bands]
+# bands = [[3, 6], [2, 7]]  # best bands with highest AUC (NER'21)
 # Number of training samples per patient. A sample is a segment.
 # # Study012-toy. Training:
 # n_samples = [[61,111]] #(preictal, interictal)
 # # Study012-toy. Testing: (16,28)
 # bands = [[3]] # best bands with highest AUC (NER'21)
 # Number of training samples per patient. A sample is a segment.
-n_samples = [[98, 1886], [64, 1980]] #(preictal, interictal)
+
 
 params['Algorithm']['metric'] = 'cosine'
 params['Algorithm']['init'] = 'random-energy'
@@ -74,18 +86,37 @@ centroid_lengths = centroid_lengths.tolist()
 # n_clusters = list(itertools.product(n_clusters, repeat=n_classes))
 # centroid_lengths = list(itertools.product(centroid_lengths, repeat=n_classes))
 
+overlap_str = 'non-overlap'
+winlen = srate * 60  # 1 minute windows extracted from epochs
+start_gap = srate * 1 # gap for random sampling
+dfname = 'split_%s_winlen-%d_gap-%d.mat' % (overlap_str, winlen, start_gap)
+
 for i_patient, patient in enumerate(patients):
-    for band in bands[i_patient]:        
+    patient = patient[0] # list -> str
+    patient_dir = os.path.join(data_dir, patient)
+    n_samples = [0] * 2 # Num. of training samples [preictal, interictal]
+    for i_condition, condition in enumerate(conditions):
+        # file names and start indices of segments
+        dirpath = os.path.join(patient_dir, condition)
+        fpath = os.path.join(dirpath, dfname)
+        i_start = utils.loadmat73(fpath, 'train_indices')
+        i_start = utils.apply2list(i_start, np.squeeze)
+        i_start = utils.apply2list(i_start, minusone)
+        n_samples[i_condition] = utils.apply2list(i_start, np.size)
+        n_samples[i_condition] = np.sum(np.array(n_samples[i_condition]))
+    
+    for band in bands[i_patient]:
         for k in n_clusters:
             for P in centroid_lengths:
                 params['Patient dir'] = os.path.join(data_dir, patient)
-                # Use same initial seed for folds of same hyperparameter point since random genetator in gen_kfold.py must be the same to generate the k-fold partition.                                
+                # Use same initial seed for folds of same hyperparameter point since random genetator in gen_kfold.py must be the same to generate the k-fold partition.
                 seed = np.random.SeedSequence()
                 params['Crossvalidation']['rng_seed'] = seed.entropy
-                rng = np.random.default_rng(seed)
-                N1, N2 = n_samples[i_patient]
-                kfold1 = utils.kfold_split(N1, n_folds, shuffle=True, rng=rng)
-                kfold2 = utils.kfold_split(N2, n_folds, shuffle=True, rng=rng)
+                rng = np.random.default_rng(seed)                
+                kfold1 = utils.kfold_split(
+                    n_samples[0], n_folds, shuffle=True, rng=rng)
+                kfold2 = utils.kfold_split(
+                    n_samples[1], n_folds, shuffle=True, rng=rng)
                 kfold1 = list(kfold1)
                 kfold2 = list(kfold2)
                 foldname = 'band%d_%s_k%d-%d_P%d-%d' % (
