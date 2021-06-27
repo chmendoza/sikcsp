7ebec0f74352f02eb7c2973091d0b1cc1853d57c
Carlos H. Mendoza-Cardenas <cmendoza@udel.edu>
2021-06-27 16:09:22 -0400

    Refactor: one single *.npy file per point

--- kcsp/crossval_reduce.py
+++ kcsp/crossval_reduce.py
@@ -33,23 +33,19 @@ centroid_lengths = [30, 40, 60, 120, 200, 350]
 method = 'regular'
 n_folds = 10
 
-MCC = np.zeros((len(n_clusters), len(centroid_lengths)))
+MCC = np.zeros((2,len(n_clusters), len(centroid_lengths)))
 for i_k, k in enumerate(n_clusters):
     for i_P, P in enumerate(centroid_lengths):        
         foldname = 'band%d_%s_k%d-%d_P%d-%d' % (
-            band, method, k, k, P, P)
-        for i_fold in range(n_folds):
-            rfname = 'crossval_fold%d_band%d_%s_k%d-%d_P%d-%d.npz' % (
-                i_fold, band, method, k, k, P, P)
-            fpath = os.path.join(data_dir, foldname, rfname)
-            with np.load(fpath) as data:
-                MCC[i_k, i_P] += data['MCC'][1] # Maximum likelihood-based score
-        MCC[i_k, i_P] /= n_folds
-        print('Average MCC for k = %d and P = %d: %.3f' \
-            % (k, P, MCC[i_k, i_P]))
+            band, method, k, k, P, P)        
+        rfname = 'crossval_band%d_%s_k%d-%d_P%d-%d.npy'\
+            % (band, method, k, k, P, P)
+        fpath = os.path.join(data_dir, foldname, rfname)
+        MCC[:,i_k, i_P] = np.load(fpath)        
 
-with np.printoptions(precision=3, floatmode='fixed'):            
-    print(MCC)
+for ii in range(2): # loop over {MAP, ML}
+    with np.printoptions(precision=3, floatmode='fixed'):            
+        print(MCC[ii])
 
-np.savetxt(os.path.join(data_dir, 'averageMCC.csv'), MCC, delimiter=",")
+np.savetxt(os.path.join(data_dir, 'averageMCC.csv'), MCC[1], delimiter=",")
 
