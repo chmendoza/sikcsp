b3172d95591038fce2cee5d2b077c883b93b54eb
Carlos H. Mendoza-Cardenas <cmendoza@udel.edu>
2021-07-01 17:44:54 -0400

    Move old stuff to private folders

--- shift_kmeans/test/context.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-# Add path to package directory to access main module using absolute import
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import si_k_means
-import datasets
